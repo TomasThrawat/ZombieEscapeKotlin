54af833f5d5396ed1bbdd7a1127344624214fa1f
TomasThrawat <tomastharwat873@gmail.com>
2026-09-21 09:42:06 +0300

    Fix API 30 lint annotation issue

--- app/src/main/java/com/tomstrawat/zombieescape/MainActivity.kt
+++ app/src/main/java/com/tomstrawat/zombieescape/MainActivity.kt
@@ -17,22 +17,17 @@ class MainActivity : Activity() {
 
     private fun hideSystemUi() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            hideSystemUiApi30()
+            window.insetsController?.let { controller ->
+                controller.hide(WindowInsets.Type.systemBars())
+                controller.systemBarsBehavior =
+                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
         } else {
             @Suppress("DEPRECATION")
             window.decorView.systemUiVisibility =
                 View.SYSTEM_UI_FLAG_FULLSCREEN or
                     View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
                     View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-        }
-    }
-
-    @android.annotation.TargetApi(Build.VERSION_CODES.R)
-    private fun hideSystemUiApi30() {
-        window.insetsController?.let { controller ->
-            controller.hide(WindowInsets.Type.systemBars())
-            controller.systemBarsBehavior =
-                WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         }
     }
 
